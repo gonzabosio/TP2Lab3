d51f5533e4f9fb673476764d4594895007970258
Federico <federicocamandone7@gmail.com>
2024-06-02 23:12:08 -0300

    add:Cuadrojuegos

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+   
+    <TableComponent />
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script  >
+import TableComponent from './components/CuadroJuegos.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TableComponent
   }
-}
+};
 </script>
 
 <style>
+h1{
+float: left;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  
 }
 </style>
