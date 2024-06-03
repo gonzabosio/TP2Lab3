ac86fe9d3f2c6a7a13c1259ab79109380c773a44
Gonzalo Bosio <gonzaabosio@gmail.com>
2024-06-03 00:37:37 -0300

    Condiciones y lista de errores

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Administrador de videojuegos</h1>
   <NuevoJuegoForm/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 import NuevoJuegoForm from './components/NuevoJuegoForm.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
     NuevoJuegoForm
   }
 }
@@ -24,6 +21,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   /* text-align: center; */
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 30px;
 }
 </style>
--- src/components/NuevoJuegoForm.vue
+++ src/components/NuevoJuegoForm.vue
@@ -4,35 +4,37 @@
         
         <form @submit.prevent="submitForm">
             <div class="form-grupo">
-                <label>Nombre</label>
+                <label>Nombre*</label>
                 <input id="nombre" type="text" name="name" v-model="nombre"/>
             </div>
             <div class="form-grupo">
-                <label>Plataforma</label>
-                <input id="plataforma" type="text" name="plataforma" list="elegirPlataforma" v-model="plataforma"/>
-                <datalist id="elegirPlataforma">
+                <label>Plataforma*</label>
+                <select id="plataforma" name="plataforma" v-model="plataforma">
                     <option>PC</option>
                     <option>PlayStation</option>
                     <option>Xbox One</option>
-            </datalist>
+                </select>
             </div>
             <div class="form-grupo">
-                <label>Estado</label>
-                <input id="estado" type="text" name="estado" list="elegirEstado" v-model="estado"/>
-                <datalist id="elegirEstado">
+                <label>Estado*</label>
+                <select id="estado" name="estado" v-model="estado">
                     <option>Pendiente</option>
                     <option>Jugando</option>
                     <option>Completado</option>
-            </datalist>
+                </select>
             </div>
             <div class="form-grupo">
                 <label>Puntaje</label>
-                <input id="puntaje" type="number" name="puntaje" max="10" min="1" value="1" v-model="puntaje">
+                <input id="puntaje" type="number" name="puntaje" max="10" min="1" v-model="puntaje">
             </div>
             <button type="submit">Registrar</button>
         </form>
+        <ul>
+            <li v-for="(error, index) in errors" :key="index">{{ error }}</li>
+        </ul>
     </div>
 </template>
+
 <script>
     export default {
         name: 'JuegoForm',
@@ -41,29 +43,44 @@
                 nombre: '',
                 plataforma: '',
                 estado: '',
-                puntaje: 1
+                puntaje: null,
+                errors: []
             };
         },
         methods: {
             submitForm() {
-                const formData = {
+                this.errors = []
+                if(this.nombre == '') {
+                    this.errors.push('Ingrese un nombre')
+                }
+                if(this.plataforma == '') {
+                    this.errors.push('Ingrese una plataforma')
+                }
+                if(this.estado == '') {
+                    this.errors.push('Ingrese el estado del juego')
+                }
+                if(this.puntaje == '') {
+                    if(!this.puntaje >= 0 || !this.puntaje <= 10) {
+                        this.errors.push('Ingrese un puntaje del 1 al 10')
+                    }
+                }
+                else {
+                    const formData = {
                     nombre: this.nombre,
                     plataforma: this.plataforma,
                     estado: this.estado,
                     puntaje: this.puntaje
+                    }
+                    console.log(formData);
+                    this.resetForm()
                 }
-                console.log(formData);
-                this.resetForm()
             },
             resetForm() {
                 this.nombre = "",
                 this.plataforma = "",
                 this.estado = "",
-                this.puntaje = 1
-            },
-            showAlert() {
-                alert('Añadir a la lista')
-            },
+                this.puntaje = null
+            }
         }
     }
 </script>
@@ -71,9 +88,18 @@
     .form-grupo {
         display: flex;
         align-items: center;
-        margin-bottom: 10px; 
+        margin-bottom: 20px; 
     }
     label {
         width: 100px;
+    }
+    input {
+        width: 190px;
+    }
+    select {
+        width: 198px;
+    }
+    ul {
+        color: red
     }
 </style>
